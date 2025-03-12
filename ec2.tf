bb808139ecb44f4722c363d1ce091e93f853f3c8
Fernando <fernandopereira-3@hotmail.com>
2025-03-12 07:52:40 -0300

    para deixar salvo

---
+++ ec2.tf
@@ -0,0 +1,125 @@
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-west-2" 
+}
+
+# Create a VPC
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+  
+  tags = {
+    Name = "main-vpc"
+  }
+}
+
+# Create a public subnet
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-west-2a"
+  
+  tags = {
+    Name = "public-subnet"
+  }
+}
+
+# Create an internet gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+  
+  tags = {
+    Name = "main-igw"
+  }
+}
+
+# Create a route table
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+  
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  
+  tags = {
+    Name = "public-route-table"
+  }
+}
+
+# Associate the route table with the subnet
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+
+# Create a security group
+resource "aws_security_group" "ec2_sg" {
+  name        = "ec2-security-group"
+  description = "Allow SSH and HTTP"
+  vpc_id      = aws_vpc.main.id
+  
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # For production, restrict to your IP
+  }
+  
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+ingress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  tags = {
+    Name = "ec2-security-group"
+  }
+}
+
+# Create an EC2 key pair - Make sure you have the public key ready
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key"
+  public_key = file("/Users/fernando/Repositorios_Git/aws_basic_ec2/deployer-key.pub")
+}
+
+# Create an EC2 instance
+resource "aws_instance" "Monitor" {
+  ami                    = "ami-0606dd43116f5ed57" # Ubuntu
+  instance_type          = "t2.micro"
+  key_name               = aws_key_pair.deployer.key_name
+  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+  subnet_id              = aws_subnet.public.id
+  
+  user_data = <<-EOF
+              #!/bin/bash
+              apt -y update
+              apt -y upgrade
+              EOF
+  
+  tags = {
+    Name = "Monitor"
+  }
+}
+
+output "IP_publico" {
+  value = aws_instance.Monitor.public_ip
+}
